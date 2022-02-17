940fd5894c3f963c296a5ebd2bed002a7f537cd5
dzwduan <dzwduan@gmail.com>
2022-02-17 21:46:16 +0800

    add part of ch3.2

--- code/src/cpu.rs
+++ code/src/cpu.rs
@@ -3,6 +3,7 @@ pub struct CPU {
     pub register_x: u8,
     pub status: u8,
     pub pc: u16,
+    pub memory: [u8; 0xffff],
 }
 
 impl CPU {
@@ -12,8 +13,57 @@ impl CPU {
             register_x: 0,
             status: 0,
             pc: 0,
+            memory: [0;0xffff],
         }
     }
+
+
+    //memory function
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos+1) as u16;
+        (hi << 8) | (lo as u16)
+    }
+
+    fn mem_write_u16(&mut self, pos:u16, data:u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos+1, hi);
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+
+        self.pc = self.mem_read_u16(0xfffc);
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+        //self.pc = 0x8000;
+        self.mem_write_u16(0xfffc, 0x8000);
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run()
+    }
+
+    pub fn run(&mut self) {
+        
+    }
+
 
     fn lda(&mut self, value: u8) {
         self.register_a = value;
