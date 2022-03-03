95bfb93e9adbb121e4a443e27330590f7a143fdc
dzwduan <dzwduan@gmail.com>
2022-03-03 22:27:29 +0800

    Update ch3.2

--- code/src/cpu.rs
+++ code/src/cpu.rs
@@ -1,135 +1,62 @@
 pub struct CPU {
     pub register_a: u8,
     pub register_x: u8,
+    pub resister_y: u8,
     pub status: u8,
-    pub pc: u16,
-    pub memory: [u8; 0xffff],
+    pub pc:     u16,
+    memory: [u8; 0xffff],
 }
 
-impl CPU {
-    pub fn new() -> Self {
-        CPU {
-            register_a: 0,
-            register_x: 0,
-            status: 0,
-            pc: 0,
-            memory: [0;0xffff],
-        }
+
+#[derive(Debug)]
+//检测驼峰命名
+#[allow(non_camel_case_types)]
+pub enum AddressingMode {
+   Immediate,
+   ZeroPage,
+   ZeroPage_X,
+   ZeroPage_Y,
+   Absolute,
+   Absolute_X,
+   Absolute_Y,
+   Indirect_X,
+   Indirect_Y,
+   NoneAddressing,
+}
+
+trait Mem {
+    fn mem_read(&self, addr: u16) -> u8;
+    fn mem_write(&mut self, addr:u16, data:u8);
+
+    fn mem_read_u16(&self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos+1) as u16;
+        (hi << 8) | lo
     }
 
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let hi = (data>>8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos+1, hi)
+    }
+}
 
-    //memory function
+
+impl Mem for CPU {
     fn mem_read(&self, addr: u16) -> u8 {
         self.memory[addr as usize]
     }
 
-    fn mem_write(&mut self, addr: u16, data: u8) {
+    fn mem_write(&mut self, addr:u16, data:u8) {
         self.memory[addr as usize] = data;
-    }
-
-    fn mem_read_u16(&mut self, pos: u16) -> u16 {
-        let lo = self.mem_read(pos) as u16;
-        let hi = self.mem_read(pos+1) as u16;
-        (hi << 8) | (lo as u16)
-    }
-
-    fn mem_write_u16(&mut self, pos:u16, data:u16) {
-        let hi = (data >> 8) as u8;
-        let lo = (data & 0xff) as u8;
-        self.mem_write(pos, lo);
-        self.mem_write(pos+1, hi);
-    }
-
-    pub fn reset(&mut self) {
-        self.register_a = 0;
-        self.register_x = 0;
-        self.status = 0;
-
-        self.pc = self.mem_read_u16(0xfffc);
-    }
-
-    pub fn load(&mut self, program: Vec<u8>) {
-        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
-        //self.pc = 0x8000;
-        self.mem_write_u16(0xfffc, 0x8000);
-    }
-
-    pub fn load_and_run(&mut self, program: Vec<u8>) {
-        self.load(program);
-        self.reset();
-        self.run()
-    }
-
-    pub fn run(&mut self) {
-        
-    }
-
-
-    fn lda(&mut self, value: u8) {
-        self.register_a = value;
-        self.udpate_zero(self.register_a);
-        self.update_nega(self.register_a);
-    }
-
-    fn tax(&mut self) {
-        self.register_x = self.register_a;
-        self.udpate_zero(self.register_x);
-        self.update_nega(self.register_x);
-    }
-
-    fn inx(&mut self) {
-        self.register_x = self.register_x.wrapping_add(1);
-        self.udpate_zero(self.register_x);
-        self.update_nega(self.register_x);
-    }
-
-    fn udpate_zero(&mut self, result: u8) {
-        if result == 0 {
-            self.status = self.status | 0b0000_0010;
-        } else {
-            self.status = self.status & 0b1111_1101;
-        }
-    }
-
-    fn update_nega(&mut self, result: u8) {
-        if result & 0b1000_0000 != 0 {
-            self.status = self.status | 0b1000_0000;
-        } else {
-            self.status = self.status & 0b0111_1111;
-        }
-    }
-
-    pub fn interpret(&mut self, program: Vec<u8>) {
-        //todo!(r#"not implemented yet"#)
-
-        self.pc = 0;
-
-        loop {
-            let opcode = program[self.pc as usize];
-            self.pc += 1;
-
-            match opcode {
-                //LDA
-                0xA9 => {
-                    let param = program[self.pc as usize];
-                    self.pc += 1;
-                    self.lda(param);
-                }
-
-                //TAX
-                0xAA => self.tax(),
-
-                0xE8 => self.inx(),
-
-                //BRK
-                0x00 => return,
-                _ => todo!(),
-            }
-        }
     }
 }
 
-#[cfg(test)]
+
+
+
+#[cfg(test)] 
 mod test {
     use super::*;
 
--- code/src/main.rs
+++ code/src/main.rs
@@ -1,6 +1,10 @@
 use crate::cpu::CPU;
 
 pub mod cpu;
+pub mod opcodes;
+
+#[macro_use]
+extern crate lazy_static;
 
 fn main() {
     let mut cpu = CPU::new();
