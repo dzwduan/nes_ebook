67afaf85e6b968fce18fe4a710d41007e150eda2
dzwduan <dzwduan@gmail.com>
2022-03-06 15:33:08 +0800

    Update rewrite code

--- code/src/nes.rs
+++ code/src/nes.rs
@@ -1,8 +1,10 @@
 
 #![allow(warnings, unused)]
 use core::slice::*;
-use std::ops::Add;
+use std::{ops::Add, collections::HashMap};
 use crate::opcode::AddrMode::{self, *};
+use crate::opcode::OpCode;
+use crate::opcode;
 
 #[derive(Debug)]
 pub struct CPU {
@@ -56,7 +58,7 @@ trait Mem {
 
     fn mem_write_u16(&mut self, addr:u16, data:u16) {
         let lo = (data & 0xff) as u8;
-        let hi = ((data << 8) & 0xff) as u8;
+        let hi = ((data >> 8) & 0xff) as u8;
 
         self.mem_write(addr, lo);
         self.mem_write(addr+1, hi);
@@ -110,17 +112,50 @@ impl CPU {
     }
 
     fn run(&mut self) {
-        let pc = 
+        //1. 识别指令
+        //2. pc+1
+        //3. 识别行为
+        //4. update pc
+        let ref opcodes: HashMap<u8, &'static OpCode> = *opcode::OpCode_Hash;
+
+        loop {
+            let hex = self.mem_read(self.pc);
+            self.pc += 1;
+            
+            let op_struct =  opcodes.get(&hex).expect(&format!("\x1b[1;31unimplemented code {:x}\x1b[0m", hex));
+
+            let pc_old = self.pc;
+
+            match hex {
+                0xa9 | 0xa5 | 0xb5 | 0xad | 0xbd | 0xb9 | 0xa1 | 0xb1 => {
+                    self.lda(&op_struct.mode);
+                    println!("exec lda");
+                },
+                 /* STA */
+                 0x85 | 0x95 | 0x8d | 0x9d | 0x99 | 0x81 | 0x91 => {
+                    self.sta(&op_struct.mode);
+                    println!("exec sta");
+                }
+                
+                0xAA => self.tax(),
+                0xe8 => self.inx(),
+                0x00 => return,
+                _ => todo!(),
+            }
+
+            //TODO:
+            self.pc += (op_struct.len - 1) as u16;
+        }
     }
 
-    pub fn load_and_run(&self, program: Vec<u8>) {
-        self.reset();
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
         self.load(program);
+        self.reset();
         self.run();
     }
 
 
-    fn get_addr(&mut self, mode:AddrMode) -> u16 {
+    fn get_addr(&mut self, mode:&AddrMode) -> u16 {
         match mode {
             Immediate    => self.pc,
             Absolute     => self.mem_read_u16(self.pc),
@@ -137,31 +172,131 @@ impl CPU {
                 let res = base.wrapping_add(self.reg_y);
                 res as u16
             },
-            Indirect     => {
-                self.mem_read_u16(self.pc)
-            }
+            Indirect     =>  self.mem_read_u16(self.pc),
+          
 
-            //TODO:
             Indirect_X   => {
                 let base = self.mem_read(self.pc);
-                let lo = base.wrapping_add(self.reg_x);
-                let hi = base.wrapping_add(1);
+                let ptr = base.wrapping_add(self.reg_x);
+                let lo = self.mem_read(ptr as u16);
+                let hi = self.mem_read(ptr.wrapping_add(1) as u16);
 
                 (hi as u16)<<8 | (lo as u16) &0xff
             },
 
-            //TODO:
             Indirect_Y   => {
                 let base = self.mem_read(self.pc);
-                let lo =base.wrapping_add(self.reg_y);
-                let hi = base.wrapping_add(1).wrapping_add(self.status.C as u8);
-                
-                (hi as u16)<<8 | (lo as u16) &0xff
+                let ptr = base.wrapping_add(self.reg_y);
+                let lo = self.mem_read(ptr as u16);
+                let hi = self.mem_read(ptr.wrapping_add(1) as u16);
+                let res_hex = (hi as u16)<<8 | (lo as u16) &0xff;
+                res_hex.wrapping_add(self.reg_y as u16)
             },
 
             //TODO:
             Relative     => self.pc + self.mem_read(self.pc) as u16,
-            Implied      => panic!("implicit address, do nothint!"),
+            Implied      => panic!("implicit address, do nothing!"),
         }
+    }
+
+    fn lda(&mut self, mode:&AddrMode) {
+        let addr = self.get_addr(mode);
+        self.reg_a = self.mem_read(addr);
+        self.update_negative_flag(self.reg_a);
+        self.update_zero_flag(self.reg_a);
+    }
+
+    fn sta(&mut self, mode:&AddrMode) {
+        let addr = self.get_addr(mode);
+        self.mem_write(addr, self.reg_a);
+    }
+
+    fn tax(&mut self) {
+        self.reg_x = self.reg_a;
+        self.update_negative_flag(self.reg_x);
+        self.update_zero_flag(self.reg_x);
+    }
+
+    fn inx(&mut self) {
+        self.reg_x = self.reg_x.wrapping_add(1);
+        self.update_negative_flag(self.reg_x);
+        self.update_zero_flag(self.reg_x);
+    }
+
+    fn update_zero_flag(&mut self, result: u8) {
+        if result == 0 {
+            self.status.Z = true;
+        } else {
+            self.status.Z = false;
+        }
+    }
+
+    fn update_negative_flag(&mut self, result: u8) {
+        let ret = result & 0b1000_0000;
+        if ret != 0 {
+            self.status.Z = true;
+        } else {
+            self.status.Z = false;
+        }
+    }
+
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_0xa9_lda_immidiate_load_data() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
+        assert_eq!(cpu.reg_a, 5);
+        // assert!(cpu.status & 0b0000_0010 == 0);
+        // assert!(cpu.status & 0b1000_0000 == 0);
+        assert!(cpu.status.Z == false);
+        assert!(cpu.status.N == false);
+    }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
+        //assert!(cpu.status & 0b0000_0010 == 0b10);
+        assert!(cpu.status.Z == true);
+    }
+
+    #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0x0A,0xaa, 0x00]);
+
+        assert_eq!(cpu.reg_x, 10)
+    }
+
+    #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+        assert_eq!(cpu.reg_x, 0xc1)
+    }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0xff, 0xaa,0xe8, 0xe8, 0x00]);
+
+        assert_eq!(cpu.reg_x, 1)
+    }
+
+    #[test]
+    fn test_lda_from_memory() {
+        let mut cpu = CPU::new();
+        cpu.mem_write(0x10, 0x55);
+
+        cpu.load_and_run(vec![0xa5, 0x10, 0x00]);
+
+        assert_eq!(cpu.reg_a, 0x55);
     }
 }
--- code/src/opcode.rs
+++ code/src/opcode.rs
@@ -5,15 +5,15 @@ use lazy_static::*;
 use AddrMode::*;
 
 #[derive(Debug)]
-pub struct Opcode {
-    code: &'static str,
-    hex: u8,
-    len: u8,
-    time : u8,
-    mode: AddrMode,
+pub struct OpCode {
+    pub code: &'static str,
+    pub hex: u8,
+    pub len: u8,
+    pub time : u8,
+    pub mode: AddrMode,
 }
 
-impl Opcode {
+impl OpCode {
     fn new(code:&'static str, hex:u8, len:u8, time:u8, mode:AddrMode) -> Self {
         Self {
             code,
@@ -46,27 +46,37 @@ pub enum AddrMode {
 
 
 lazy_static! {
-    pub static ref OPCODE_TAB: Vec<Opcode> = vec![
+    pub static ref OpCode_TAB: Vec<OpCode> = vec![
     //BRK
-        Opcode::new("BRK", 0x00, 1,7, Implied),
+        OpCode::new("BRK", 0x00, 1, 7, Implied),
+        OpCode::new("TAX", 0xaa, 1, 2, Implied),
+        OpCode::new("INX", 0xe8, 1, 2, Implied),
 
     //LDA
-        Opcode::new("LDA", 0xa9, 2,3, Immediate),
-        Opcode::new("LDA", 0xa5, 2,3, ZeroPage),
-        Opcode::new("LDA", 0xb5, 2,4, ZeroPage_X),
-        Opcode::new("LDA", 0xad, 3,4, Absolute),
-        Opcode::new("LDA", 0xbd, 3,4, Absolute_X),
-        Opcode::new("LDA", 0xb9, 3,4, Absolute_Y),
-        Opcode::new("LDA", 0xa1, 2,6, Indirect_X),
-        Opcode::new("LDA", 0xb1, 2,5, Indirect_Y),
+        OpCode::new("LDA", 0xa9, 2, 3, Immediate),
+        OpCode::new("LDA", 0xa5, 2, 3, ZeroPage),
+        OpCode::new("LDA", 0xb5, 2, 4, ZeroPage_X),
+        OpCode::new("LDA", 0xad, 3, 4, Absolute),
+        OpCode::new("LDA", 0xbd, 3, 4, Absolute_X),
+        OpCode::new("LDA", 0xb9, 3, 4, Absolute_Y),
+        OpCode::new("LDA", 0xa1, 2, 6, Indirect_X),
+        OpCode::new("LDA", 0xb1, 2, 5, Indirect_Y),
+
+        OpCode::new("STA", 0x85, 2, 3, ZeroPage),
+        OpCode::new("STA", 0x95, 2, 4, ZeroPage_X),
+        OpCode::new("STA", 0x8d, 3, 4, Absolute),
+        OpCode::new("STA", 0x9d, 3, 5, Absolute_X),
+        OpCode::new("STA", 0x99, 3, 5, Absolute_Y),
+        OpCode::new("STA", 0x81, 2, 6, Indirect_X),
+        OpCode::new("STA", 0x91, 2, 6, Indirect_Y),
     ];
 
 
-    //Opcode是全局存在的
-    pub static ref Opcode_Hash: HashMap<u8, &'static Opcode> = {
+    //OpCode是全局存在的
+    pub static ref OpCode_Hash: HashMap<u8, &'static OpCode> = {
         let mut kv = HashMap::new();
 
-        for op in &*OPCODE_TAB {
+        for op in &*OpCode_TAB {
             kv.insert(op.hex, op);
         }
         kv
